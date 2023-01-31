92c922c394478ff1153e15b6c3fc86d559b1b025
DESKTOP-SBMED09\frog <sweetpoteto@naver.com>
2023-01-31 15:46:55 +0900

    cctv_page UI 변경

---
+++ jikimi/static/cctv_page/script.js
@@ -0,0 +1,18 @@
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(){
+  document.querySelector("body").classList.toggle("active");
+})
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+
+$(document).on('click', '.video-item', function(e){
+	alert("isWorking?");
+	$("video").attr("src", $(this).attr("id"));	
+	$("video")[0].load();				// 위 함수를 호출해야 영상 소스를 바꿔서 재생 가능
+	document.getElementById("video").play();
+})
