9f00ca457a76b094441c2b34d097ff07f24b6cea
RATOA <110943728+RATOA@users.noreply.github.com>
2023-02-03 11:26:39 +0900

    Add files via upload

---
+++ select-box test/script.js
@@ -0,0 +1,26 @@
+let loginForm = document.querySelector('.login-wrap');
+let signupForm = document.querySelector('.signup-wrap');
+let title = document.querySelector('title');
+
+let signupToggleBtn = document.querySelector('#toggle-signup');
+let loginToggleBtn = document.querySelector('#toggle-login');
+
+signupToggleBtn.onclick = () => {
+    loginForm.classList.remove('active');
+    signupForm.classList.add('active');
+    title.textContent = 'Signup form';
+}
+
+loginToggleBtn.onclick = () => {
+    signupForm.classList.remove('active');
+    loginForm.classList.add('active');
+    title.textContent = 'Login form';
+}
+
+const wrapper = document.querySelector(".wrapper"),
+selectBtn = wrapper.querySelector(".select-btn");
+
+selectBtn.addEventListener("click", () => {
+  wrapper.classList.toggle("active");
+});
+
