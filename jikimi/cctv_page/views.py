168dfb81b2afde49a1f79d66943fdb675a9f6b7f
DESKTOP-SBMED09\frog <sweetpoteto@naver.com>
2023-02-14 17:23:35 +0900

    navbar 수정, cctv_page get 파라미터 적용

--- jikimi/cctv_page/views.py
+++ jikimi/cctv_page/views.py
@@ -10,6 +10,13 @@ def cctv_page(request):
     list = {
         "videoList" : videoList 
     }
+
+    url = request.GET.get("video_url")
+
+    if(url != None):
+        list['video_url'] = url
+        print(list['video_url'])
+        
     
     return render(request, 'cctv_page/cctv.html', list)
 
